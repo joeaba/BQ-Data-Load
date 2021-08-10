088d5007667f83d98c09db92b4e68b189c8b63f3
joeaba <77398477+joeaba@users.noreply.github.com>
2021-08-10 13:30:39 +0530

    updated script to pass credentials and variables

--- script/copy_new_scripting2.py
+++ script/copy_new_scripting2.py
@@ -1,10 +1,9 @@
 import os
 import sys
-
 fhand1 = os.popen('pwd')
 output10 = fhand1.readlines()
 output11 = output10[0].strip('\n')
-print(output11)
+fhand1.close()
 
 os.system('gcloud config configurations activate mainnet-config')
 cred = str(output11) + '/gcpcmdlineuser_mainnet.json'
@@ -13,19 +12,23 @@ os.system('gcloud auth activate-service-account anmol-bigtable-test@mainnet-beta
 
 file1 = open("SOL transaction data load into BQ - accounts.txt")
 output = file1.readlines()
-print(output)       #['HWFzm33hjwjHQB24np1bbW6oPy1xo8kGVfBe2cQyYM7E\n', 'A5x1aLP7ribPDuRNXcMaTAzwnyn3mFqK5Xewen4Zk    3q1\n']
-print('check1')
 file1.close()
+
 for i in range(len(output)):
     output[i] = output[i].strip('\n')
-    print(output[i])      #print single account at a time     HWFzm33hjwjHQB24np1bbW6oPy1xo8kGVfBe2cQyYM7E
-    print('check2')
-print (output)
+
 def default_cred():
+    fhand1 = os.popen('pwd')
+    output10 = fhand1.readlines()
+    output11 = output10[0].strip('\n')
     os.system('gcloud config configurations activate default')
+    cred = str(output11) + '/gcpcmdlineuser.json'
     os.environ['GOOGLE_APPLICATION_CREDENTIALS']=cred
     os.system('gcloud auth activate-service-account gcp-service-account@principal-lane-200702.iam.gserviceaccount.com --key-file='+cred)
 def mainnet_cred():
+    fhand1 = os.popen('pwd')
+    output10 = fhand1.readlines()
+    output11 = output10[0].strip('\n')
     os.system('gcloud config configurations activate mainnet-config')
+    cred = str(output11) + '/gcpcmdlineuser_mainnet.json'
     os.environ['GOOGLE_APPLICATION_CREDENTIALS']=cred
-    os.system('gcloud auth activate-service-account anmol-bigtable-test@mainnet-beta.iam.gserviceaccount.com --key-file='+cred)
