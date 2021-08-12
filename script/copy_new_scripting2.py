1bd01e1907dfd54d6967cb47fb8a1000156d5087
joeaba <77398477+joeaba@users.noreply.github.com>
2021-08-13 00:12:16 +0530

    removed the SOL Transaction in the file

--- script/copy_new_scripting2.py
+++ script/copy_new_scripting2.py
@@ -10,13 +10,6 @@ cred = str(output11) + '/gcpcmdlineuser_mainnet.json'
 os.environ['GOOGLE_APPLICATION_CREDENTIALS']= cred
 os.system('gcloud auth activate-service-account anmol-bigtable-test@mainnet-beta.iam.gserviceaccount.com --key-file='+ cred)
 
-file1 = open("SOL transaction data load into BQ - accounts.txt")
-output = file1.readlines()
-file1.close()
-
-for i in range(len(output)):
-    output[i] = output[i].strip('\n')
-
 def default_cred():
     fhand1 = os.popen('pwd')
     output10 = fhand1.readlines()
