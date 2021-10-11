5cc44df02f1ca705a5d0856e64ec8d1dea5c5156
joeaba <77398477+joeaba@users.noreply.github.com>
2021-10-12 03:16:03 +0530

    updating the script with commands to set service account and project

--- script/Setup_Credential.py
+++ script/Setup_Credential.py
@@ -6,6 +6,8 @@ output11 = output10[0].strip('\n')
 fhand1.close()
 
 os.system('gcloud config configurations activate mainnet-config')
+os.system('gcloud config set project mainnet-beta')
+os.system('gcloud config set account anmol-bigtable-test@mainnet-beta.iam.gserviceaccount.com')
 cred = str(output11) + '/gcpcmdlineuser_mainnet.json'
 os.environ['GOOGLE_APPLICATION_CREDENTIALS']= cred
 os.system('gcloud auth activate-service-account anmol-bigtable-test@mainnet-beta.iam.gserviceaccount.com --key-file='+ cred)
@@ -15,6 +17,8 @@ def default_cred():
     output10 = fhand1.readlines()
     output11 = output10[0].strip('\n')
     os.system('gcloud config configurations activate default')
+    os.system('gcloud config set project principal-lane-200702')
+    os.system('gcloud config set account gcp-service-account@principal-lane-200702.iam.gserviceaccount.com')
     cred = str(output11) + '/gcpcmdlineuser.json'
     os.environ['GOOGLE_APPLICATION_CREDENTIALS']=cred
     os.system('gcloud auth activate-service-account gcp-service-account@principal-lane-200702.iam.gserviceaccount.com --key-file='+cred)
@@ -23,5 +27,7 @@ def mainnet_cred():
     output10 = fhand1.readlines()
     output11 = output10[0].strip('\n')
     os.system('gcloud config configurations activate mainnet-config')
+    os.system('gcloud config set project mainnet-beta')
+    os.system('gcloud config set account anmol-bigtable-test@mainnet-beta.iam.gserviceaccount.com')
     cred = str(output11) + '/gcpcmdlineuser_mainnet.json'
     os.environ['GOOGLE_APPLICATION_CREDENTIALS']=cred
