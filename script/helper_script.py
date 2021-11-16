8150e31cef3c564d1fecbc255be105b80fcbcf58
joeaba <77398477+joeaba@users.noreply.github.com>
2021-11-16 22:34:14 +0530

    updated the file

--- script/helper_script.py
+++ script/helper_script.py
@@ -2,22 +2,20 @@ import os
 import shutil
 from os import path
 import datetime
-
 TIME_ = datetime.datetime.now()
 def make_copy():
-  if path.exists("last transac.txt"):
+  if path.exists("last_transac.txt"):
     # get the path to the file in the current directory
-    src = path.realpath("last transac.txt")
+    src = path.realpath("last_transac.txt")
     #seperate the path from the filter
     head, tail = path.split(src)
     file_name, Extension = tail.split('.')
     #let's make a backup copy by appending "bak" to the name
-    dst = head + "/" + file_name + "_" + str(TIME_)+ "." + Extension
+    dst = head.strip('\'') + "/" + file_name.strip('\'') + "_" + str(TIME_)+ "." + Extension.strip('\'')
     # nowuse the shell to make a copy of the file
-    shutil.copy(src, dst)
+    shutil.copy(src, dst.strip('\''))
     #copy over the permissions,modification
     shutil.copystat(src,dst)
-
 if __name__=="__main__":
   #TIME_ = datetime.datetime.now()
   main()
